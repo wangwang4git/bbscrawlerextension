988e3294482eec9dc96f84084e0e1f4c73b98baf
victorwwang <victorwwang@tencent.com>
2019-02-24 23:07:51 +0800

    1. 修改后台执行逻辑。

--- res/js/background.js
+++ res/js/background.js
@@ -1,30 +1,51 @@
 'use strict';
 
+// for debug
 let counter = 0;
 let interval_ref;
 
 $(function () {
     console.log('hello background...');
 
+    // 2s执行一次爬取任务
     interval_ref = setInterval(function () {
         bbs_crawler();
     }, 2000);
+
 });
 
 function bbs_crawler() {
+    let keyword = '家具';
+    let url = 'http://bbs.oa.com/forum/search?k=' + keyword + '&page=1';
+    url = encodeURI(url);
     $.ajax({
-        url: 'http://bbs.oa.com/forum/search?k=%E5%AE%B6%E5%85%B7',
+        url: url,
         type: 'GET',
         success: function (data) {
             update_index();
 
             console.log(data);
+
+            // let html = $.parseHTML(data);
+
+            $.post('http://127.0.0.1:5000/bbscrawler',
+                {
+                    status: '0',
+                    html: data
+                },
+                function (data, status) {
+                    console.log("数据: \n" + data + "\n状态: " + status);
+                });
         },
         error: function (XMLHttpRequest, textStatus, errorThrown) {
             update_index();
 
             // 状态码
             console.log(XMLHttpRequest.status);
+            if (XMLHttpRequest.status === 502) {
+                // 错误信息同步
+                console.log('错误信息同步');
+            }
             // 状态
             console.log(XMLHttpRequest.readyState);
             // 错误信息
@@ -35,7 +56,7 @@ function bbs_crawler() {
 
 function update_index() {
     counter++;
-    if (counter >= 11) {
+    if (counter >= 1) {
         clearInterval(interval_ref);
         return;
     }
